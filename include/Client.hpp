01d85cd71689f704b4037ae88066187cf593964f
Bvernimm <bazvernim@outlook.fr>
2023-07-04 11:41:14 +0200

    user mode work in a cleaner way now

--- include/Client.hpp
+++ include/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: bvernimm <bvernimm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 22:25:55 by root              #+#    #+#             */
-/*   Updated: 2023/07/04 11:10:11 by bvernimm         ###   ########.fr       */
+/*   Updated: 2023/07/04 11:39:57 by bvernimm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,55 +28,41 @@ class Client
 		void		sendToClient(std::string str); // write a string on the client's fd
 		std::string	receiveFromClient(); // receive and return a string written by the client
 
+		/* mode function */
+		int		addMode(char mode); // add an user mode, return 0 if it went well
+		int		rmMode(char mode); // remove an user mode, return 0 if it went well
+		bool	hasMode(char mode); // search if client has mode activated
+
 		/* setters */
 		void		setNickname(std::string nickName);
 		void		setUsername(std::string userName);
-		void		setInvisible(bool mode);
-		void		setWallops(bool mode);
-		void		setOperator(bool mode);
-		void		setServerNotice(bool mode);
-		void		setHiddenHost(bool mode);
-		void		setAway(bool mode);
-		void		setRestricted(bool mode);
-		void		setQuiet(bool mode);
-		void		setBot(bool mode);
-		void		setDeaf(bool mode);
-		void		setCallerID(bool mode);
 		
 		/* getters */
 		int			getFd();
 		std::string	getNickname();
 		std::string	getUsername();
-		bool		isInvisible();
-		bool		isWallops();
-		bool		isOperator();
-		bool		isServerNotice();
-		bool		isHiddenHost();
-		bool		isAway();
-		bool		isRestricted();
-		bool		isQuiet();
-		bool		isBot();
-		bool		isDeaf();
-		bool		isCallerID();
         
     private:
         int         _fd;
         std::string _nickname;
         std::string _username;
-
-		/* user modes */  
-		/* !!! We can choose to add/remove modes, those are just the more common one !!! */
-		bool		_invisible; // hides the user from WHO and NAMES lists
-		bool		_wallops; //  receives "wallops" messages, which are system-wide messages sent by operators
-		bool		_operator; // grants operator privileges to the user
-		bool		_serverNotice; // receives server notices
-		bool		_hiddenHost; //  hides the user's host information
-		bool		_away; //  indicates that the user is currently afk
-		bool		_restricted; //  restricts private messages from unregistered or unidentified users
-		bool		_quiet; //  prevents a user from receiving channel messages or private messages from users who are not on their access list or buddy list
-		bool		_bot; //  marks the user as a bot
-		bool		_deaf; //  disables the user's ability to receive channel messages, they user can still send messages but won't receive any messages from others
-		bool		_callerID; //   enables the user to see the IP address or hostname of other users in private messages or channel user lists
+		std::string _modesList;
 };
+
+/* all user modes currently added : 
+		
+	invisible (i) : hides the user from WHO and NAMES lists
+	wallops (w) : receives "wallops" messages, which are system-wide messages sent by operators
+	operator (o) : grants operator privileges to the user
+	serverNotice (s) : receives server notices
+	hiddenHost (x) : hides the user's host information
+	away (a) : indicates that the user is currently afk
+	restricted (r) : restricts private messages from unregistered or unidentified users
+	quiet (q) : prevents a user from receiving channel messages or private messages from users who are not on their access list or buddy list
+	bot (b) : marks the user as a bot
+	deaf (D) : disables the user's ability to receive channel messages, they user can still send messages but won't receive any messages from others
+	callerID (G) : enables the user to see the IP address or hostname of other users in private messages or channel user lists
+		
+!!! We can choose to add/remove modes, those are just the more common one !!! */
 
 #endif
--- src/Client.cpp
+++ src/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: bvernimm <bvernimm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 13:27:06 by root              #+#    #+#             */
-/*   Updated: 2023/07/04 11:13:14 by bvernimm         ###   ########.fr       */
+/*   Updated: 2023/07/04 11:40:28 by bvernimm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 Client::Client() {
 }
 
-Client::Client(int fd) : _fd(fd), _nickname("DefaultNickname"), _invisible(false), _wallops(false), _operator(false), _serverNotice(false), _hiddenHost(false), _away(false), _restricted(false), _quiet(false), _bot(false), _deaf(false), _callerID(false)
+Client::Client(int fd) : _fd(fd), _nickname("DefaultNickname")
 {
 	std::stringstream ss;
 	
@@ -54,33 +54,47 @@ std::string	Client::receiveFromClient()
 	return (msg);
 }
 
+/* mode function */
+int	Client::addMode(char mode)
+{
+	if (mode == 'i' || mode == 'w' || mode == 'o' || mode == 's' || mode == 'x' || mode == 'a' || mode == 'r' || mode == 'q' || mode == 'b' || mode == 'D' || mode == 'G') // list of existing modes
+	{
+		if (_modesList.find(mode) == std::string::npos)
+			_modesList = _modesList + mode;
+		else
+			return (1); // error, mode is already activated
+	}
+	else
+		return (-1); // error, mode invalid
+	return (0);
+}
+	
+void	Client::rmMode(char mode)
+{
+	if (mode == 'i' || mode == 'w' || mode == 'o' || mode == 's' || mode == 'x' || mode == 'a' || mode == 'r' || mode == 'q' || mode == 'b' || mode == 'D' || mode == 'G') // list of existing modes
+	{
+		if (_modesList.find(mode) != std::string::npos)
+			_modesList.erase(_modesList.find(mode), 1);
+		else
+			return (1); // error, mode is not activated
+	}
+	else
+		return (-1); // error, mode invalid
+	return (0);
+}
+
+bool Client::hasMode(char mode)
+{
+	if (_modesList.find(mode) == std::string::npos)
+		return (false)
+	return (true);
+}
+
 /* setters */
 void Client::setNickname(std::string nickName) { this->_nickname = nickName; }
 void Client::setUsername(std::string userName) { this->_username = userName; }
-void Client::setInvisible(bool mode) { this->_invisible = mode; }
-void Client::setWallops(bool mode) { this->_wallops = mode; }
-void Client::setOperator(bool mode) { this->_operator = mode; }
-void Client::setServerNotice(bool mode) { this->_serverNotice = mode; }
-void Client::setHiddenHost(bool mode) { this->_hiddenHost = mode; }
-void Client::setAway(bool mode) { this->_away = mode; }
-void Client::setRestricted(bool mode) { this->_restricted = mode; }
-void Client::setQuiet(bool mode) { this->_quiet = mode; }
-void Client::setBot(bool mode) { this->_bot = mode; }
-void Client::setDeaf(bool mode) { this->_deaf = mode; }
-void Client::setCallerID(bool mode) { this->_callerID = mode; }
 
 /* getters */
 int			Client::getFd() { return (this->_fd); }
 std::string	Client::getNickname() { return (this->_nickname); }
 std::string	Client::getUsername() { return (this->_username); }
-bool		Client::isInvisible() { return (this->_invisible); }
-bool		Client::isWallops() { return (this->_wallops); }
-bool		Client::isOperator() { return (this->_operator); }
-bool		Client::isServerNotice() { return (this->_serverNotice); }
-bool		Client::isHiddenHost() { return (this->_hiddenHost); }
-bool		Client::isAway() { return (this->_away); }
-bool		Client::isRestricted() { return (this->_restricted); }
-bool		Client::isQuiet() { return (this->_quiet); }
-bool		Client::isBot() { return (this->_bot); }
-bool		Client::isDeaf() { return (this->_deaf); }
-bool		Client::isCallerID() { return (this->_callerID); }
