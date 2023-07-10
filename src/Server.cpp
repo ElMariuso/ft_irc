8282a1ba40bf04f008983c5f0ce317fa578ebf5b
ElMariuso <thiry.marius@gmail.com>
2023-07-10 17:19:56 +0200

    Back from scratch

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 21:42:57 by root              #+#    #+#             */
-/*   Updated: 2023/07/10 16:59:34 by root             ###   ########.fr       */
+/*   Updated: 2023/07/10 17:19:38 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,99 +29,6 @@ Server::~Server() {}
 /* Functions */
 int Server::processServer()
 {
-    // int             ret;
-    int             ready;
-    struct pollfd   serverPfd;
-
-    serverPfd.fd = this->serverSocket;
-    serverPfd.events = POLLIN;
-    this->fds.push_back(serverPfd);
-    do
-    {
-        std::cout << "Waiting for poll()..." << std::endl;
-        ready = poll(this->fds.data(), this->fds.size(), -1);
-        if (ready < 0)
-            return (-1);
-        std::cout << "Checking for new connections..." << std::endl;
-        // ret = this->manageNewConnection();
-        // if (ret < 0)
-        //     return (-2);
-        // this->receiveMessage();
-        // this->manageLogout();
-    } while (true);
-    return (0);
-}
-
-int Server::manageNewConnection()
-{
-    struct sockaddr_in  clientAddr;
-    socklen_t           clientAddrLen;
-    int                 new_socket;
-    struct pollfd       clientPfd;
-
-    if (this->fds[0].revents & POLLIN)
-    {
-        /* New connection */
-        clientAddrLen = sizeof(clientAddr);
-        new_socket = accept(this->serverSocket, (struct sockaddr*)&clientAddr, &clientAddrLen);
-        std::cout << "Establishing new connection: " << new_socket << std::endl;
-        if (new_socket >= 0)
-        {
-            clientPfd.fd = new_socket;
-            clientPfd.events = POLLIN;
-            this->fds.push_back(clientPfd);
-            this->clientsList[new_socket] = new Client();
-        }
-        else
-            return (-1);
-    }
-    return (0);
-}
-
-int Server::receiveMessage()
-{
-    std::size_t index;
-    int         client_socket;
-    char        buffer[1024];
-    int         bytes_read;
-
-    index = 1;
-    while (index < this->fds.size())
-    {
-        if (this->fds[index].revents & POLLIN)
-        {
-            client_socket = this->fds[index].fd;
-            bytes_read = recv(client_socket, buffer, sizeof(buffer), 0);
-            if (bytes_read <= 0)
-            {
-                // Error
-            }
-            else
-            {
-                
-            }
-        }
-    }
-    return (0);
-}
-
-int Server::manageLogout()
-{
-    std::map<int, Client*>::iterator    it;
-    int                                 client_socket;
-    
-    it = this->clientsList.begin();
-    while (it != this->clientsList.end()) /* Logout */
-    {
-        client_socket = it->first;
-        if (this->fds[client_socket].revents & POLLHUP)
-        {
-            std::cout << "Logout: " << client_socket << std::endl;
-            this->fds.erase(this->fds.begin() + client_socket);
-            this->clientsList.erase(it++);
-        }
-        ++it;
-    }
     return (0);
 }
 
