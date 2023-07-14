08aee7cdf4044d8c839cc5fe907f174a892b300e
ElMariuso <thiry.marius@gmail.com>
2023-07-14 20:11:49 +0200

    Right format

--- src/Command.cpp
+++ src/Command.cpp
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 15:32:31 by mthiry            #+#    #+#             */
-/*   Updated: 2023/07/13 00:50:07 by mthiry           ###   ########.fr       */
+/*   Updated: 2023/07/14 19:43:08 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,9 +77,7 @@ void Command::nickMessages(Server &server, Client *client, std::string newNickna
 void Command::msgMessages(Server &server, Client *src, Client *dest, std::string message)
 {
     (void)server;
-    std::string msg001_1 = ":" + src->getNickname() + "!" + src->getUsername() + "@" + src->getHostname();
-    std::string msg001_2 = " RPL_PRIVMSG " + dest->getNickname() + " :" + message + "\r\n";
-    std::string msg001 = msg001_1 + msg001_2;
+    std::string msg001 = ":" + src->getNickname() + " PRIVMSG " + dest->getNickname() + " :" + message + "\r\n";
 
     src->sendToFD(msg001);
     dest->sendToFD(msg001);
