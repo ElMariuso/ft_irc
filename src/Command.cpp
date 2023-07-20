59331540a33ffb54deb2b683f6bffc27829bcfe6
ElMariuso <thiry.marius@gmail.com>
2023-07-20 07:50:30 +0200

    Fixes

--- src/Command.cpp
+++ src/Command.cpp
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 15:32:31 by mthiry            #+#    #+#             */
-/*   Updated: 2023/07/20 07:44:49 by mthiry           ###   ########.fr       */
+/*   Updated: 2023/07/20 07:47:27 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -208,7 +208,7 @@ void Command::kick(const Server &server, const Client &src, Client *dest, const 
     /* Get the connected list */
     const std::map<int, Client*>    &connected = channel->getConnected();
 
-    if (channel->findConnectedByName(destName) != connected.end()) /* ERR_USERNOTINCHANNEL (441) */
+    if (channel->findConnectedByName(destName) == connected.end()) /* ERR_USERNOTINCHANNEL (441) */
         src.sendToFD(Message::err_usernotinchannel_441(serverName, destName, channelName));
     else if (channel->findConnectedByName(srcName) == connected.end()) /* ERR_NOTONCHANNEL (442) */
         src.sendToFD(Message::err_notonchannel_442(serverName, srcName, channelName));
