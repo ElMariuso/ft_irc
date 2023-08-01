809dfdfe159fd2467f741b50db8e7cfc55b29f8a
ElMariuso <thiry.marius@gmail.com>
2023-08-01 22:08:26 +0200

    No error of no registeration for ping

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 21:42:57 by root              #+#    #+#             */
-/*   Updated: 2023/07/27 21:28:52 by mthiry           ###   ########.fr       */
+/*   Updated: 2023/08/01 22:06:15 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -292,7 +292,7 @@ void Server::getMessages(const std::string &message, Client *client)
             this->withoutAuthentication(command, client, args[0]);
         if (client && client->getIsConnected() && client->getIsAuthenticated()) /* Process commands that require authentication */
             this->withAuthentication(command, client, args);
-        else if (client && client->getIsConnected() && !client->getIsAuthenticated()) /* If the client is connected but not authenticated, send ERR_NOTREGISTERED(451) to the client */
+        else if (client && client->getIsConnected() && !client->getIsAuthenticated() && command.getType() != PING) /* If the client is connected but not authenticated, send ERR_NOTREGISTERED(451) to the client */
             client->sendToFD(this->err_notregistered_451(client->getNickname()));
     }
     else if (command.getType() == UNKNOW) /* If the command type is unknown */
