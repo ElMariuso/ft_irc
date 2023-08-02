4677923e3f77f8f45e1e821da0f16fc2bfa3b06a
ElMariuso <thiry.marius@gmail.com>
2023-08-02 16:05:44 +0200

    No comments

--- include/Client.hpp
+++ include/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 22:25:55 by root              #+#    #+#             */
-/*   Updated: 2023/08/02 00:08:16 by mthiry           ###   ########.fr       */
+/*   Updated: 2023/08/02 16:05:36 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,21 +81,5 @@ class Client
 		std::time_t	lastPingTime;
 		std::string	savedCommand;
 };
-
-/* all user modes currently added : 
-		
-	invisible (i) : hides the user from WHO and NAMES lists
-	wallops (w) : receives "wallops" messages, which are system-wide messages sent by operators
-	operator (o) : grants operator privileges to the user
-	serverNotice (s) : receives server notices
-	hiddenHost (x) : hides the user's host information
-	away (a) : indicates that the user is currently afk
-	restricted (r) : restricts private messages from unregistered or unidentified users
-	quiet (q) : prevents a user from receiving channel messages or private messages from users who are not on their access list or buddy list
-	bot (b) : marks the user as a bot
-	deaf (D) : disables the user's ability to receive channel messages, they user can still send messages but won't receive any messages from others
-	callerID (G) : enables the user to see the IP address or hostname of other users in private messages or channel user lists
-		
-!!! We can choose to add/remove modes, those are just the more common one !!! */
 
 #endif
