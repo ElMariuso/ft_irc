3fffbc06661f7a864ba38c8d46fa40b5d13877b3
ElMariuso <thiry.marius@gmail.com>
2023-08-11 00:28:44 +0200

    NO FUCKING DEBUG

--- src/Command.cpp
+++ src/Command.cpp
@@ -6,7 +6,7 @@
 /*   By: mthiry <mthiry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 15:32:31 by mthiry            #+#    #+#             */
-/*   Updated: 2023/08/10 20:16:49 by mthiry           ###   ########.fr       */
+/*   Updated: 2023/08/11 00:28:35 by mthiry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -771,8 +771,6 @@ void Command::setType()
         type = this->message.substr(0, this->message.find(' '));
         this->message.erase(0, pos + 1);
     }
-
-    std::cout << "TYPE: <" << type << ">" << std::endl;
 
     if (type == "PASS") /* Authentification */
         this->type = PASS;
